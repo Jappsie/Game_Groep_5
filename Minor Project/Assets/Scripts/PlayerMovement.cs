cc3656ca2b90042e8c01c7e26f355636f93060f9
Jasper van Bemmelen <jaspervanbemmelen@wlan-145-94-178-193.wlan.tudelft.nl>
2016-11-18 17:29:30 +0100

    Eerste versie volledig project

---
+++ Minor Project/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMovement : MonoBehaviour {
+
+    private Rigidbody rb;
+    public float speed;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+	
+	void FixedUpdate () {
+        float horizontalMovement = Input.GetAxis("Horizontal");        
+        float verticalMovement = Input.GetAxis("Vertical");
+        Vector3 movement = new Vector3(horizontalMovement, 0.0f, verticalMovement);
+
+        rb.AddForce(movement * speed);
+
+    }
+
+    
+}
