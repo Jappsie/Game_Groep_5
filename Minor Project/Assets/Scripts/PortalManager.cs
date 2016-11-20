9d6588b526ba5a3a47b7a8aa4047dbc6c0288985
MPapendrecht <M.N.G.Papendrecht@student.tudelft.nl>
2016-11-20 14:39:21 +0100

    SceneManagement
    Ik heb een portal georienteerde versie van de scenemanagement gemaakt.
    Alles gaat nu via de inspector, geen ingewikkelde dingen meer. LET OP!
    Ik kan niet controleren of je een Scene in de 'Scene' variabele stopt
    (nog geen API voor), dus als je er per ongeluk een camera in sleept
    geeft hij dat niet meteen aan. De rest wijst zich vanzelf denk ik.

---
+++ Minor Project/Assets/Scripts/PortalManager.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PortalManager : MonoBehaviour {
+
+    public Object Scene;
+    public bool Additive;
+    public Vector3 Position;
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            string portalName = gameObject.name;
+            int Portal = int.Parse(portalName.Replace("Portal ", ""));
+            SceneManagerScript.goToScene(Scene.name, Additive);
+            other.transform.position = Position + new Vector3(0, other.transform.position.y, 0);
+        }
+    }
+
+}
---
+++ Minor Project/Assets/Scripts/SceneManagerScript.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class SceneManagerScript : MonoBehaviour {
+
+    static SceneManagerScript SceneManagementInstance;
+    public Object MainCamera;
+
+    
+    void Awake()
+    {
+        // Avoid duplcates in DontDestroyOnLoad scene
+        if (SceneManagementInstance != null)
+        {
+            Destroy(gameObject);
+            Destroy(MainCamera);
+        }
+        else
+        {
+            DontDestroyOnLoad(gameObject);
+            DontDestroyOnLoad(MainCamera);
+            SceneManagementInstance = this;
+        }
+    }
+
+    public static void goToScene(string scene, bool Additive)
+    {
+        LoadSceneMode mode = LoadSceneMode.Single;
+        if (Additive)
+        {
+            mode = LoadSceneMode.Additive;
+        }
+        SceneManager.LoadScene(scene, mode);
+    }
+}
