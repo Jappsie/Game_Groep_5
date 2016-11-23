1af63ab7507f8a09f2eaccd442e592be8ed9be95
Fieke Thijssen <f.m.w.thijssen@student.tudelft.nl>
2016-11-23 14:01:09 +0100

    (Basic) Floating implemented

--- Minor Project/Assets/Scripts/PlayerMovement.cs
+++ Minor Project/Assets/Scripts/PlayerMovement.cs
@@ -5,53 +5,57 @@ using System;
 public class PlayerMovement : HealthSystem
 {
 
-    public float speed = 10f;       // Speed variable
-    public float gravity = 14.0f;    // Gravity effect
-    public float jumpForce = 10.0f;  // Force of jump
+	public float speed = 10f;       // Speed variable
+	public float gravity = 14.0f;    // Gravity effect
+	public float jumpForce = 1.0f;  // Force of jump
+	public float zweefConstant = 5.0f;	//Gravity effect during float
 
-    private CharacterController controller;     // Controller of the movement
-    private float verticalVelocity;            // Velocity regarding jump/gravity
-    private Quaternion rotation;                // Align movement with camera position
+	private CharacterController controller;     // Controller of the movement
+	private float verticalVelocity;            // Velocity regarding jump/gravity
+	private Quaternion rotation;                // Align movement with camera position
 
-    // Get reference to the CharacterController
-    void Start()
-    {
-        controller = GetComponent<CharacterController>();
-        Vector3 camera = GameObject.FindGameObjectWithTag( "MainCamera" ).transform.position;
-        camera.y = gameObject.transform.position.y;
-        rotation = Quaternion.LookRotation( transform.position-camera, Vector3.up );
-    }
+	// Get reference to the CharacterController
+	void Start()
+	{
+		controller = GetComponent<CharacterController>();
+		Vector3 camera = GameObject.FindGameObjectWithTag( "MainCamera" ).transform.position;
+		camera.y = gameObject.transform.position.y;
+		rotation = Quaternion.LookRotation( transform.position-camera, Vector3.up );
+	}
 
-    // Make the object move
-    void Update()
-    {
-        // Jump if not floating
-        if ( controller.isGrounded )
-        {
-            verticalVelocity = 0.0f;
-            if ( Input.GetKey( KeyCode.Space ) )
-            {
-                verticalVelocity = jumpForce;
-            }
-        }
-        else
-        {
-            verticalVelocity -= gravity * Time.deltaTime;
-        }
+	// Make the object move
+	void Update()
+	{
+		// Jump if not floating
+		if (controller.isGrounded)
+		{
+			verticalVelocity = 0.0f;
+			if ( Input.GetKey( KeyCode.Space ) )
+			{
+				verticalVelocity = jumpForce;
+			}
+		}
+		else
+		{
+			if (Input.GetKey (KeyCode.Space) && verticalVelocity < 0) {
+				verticalVelocity -= zweefConstant * Time.deltaTime;
+			} else {
+				verticalVelocity -= gravity * Time.deltaTime;
+			}
+		}
 
-        // Move about in the 2D area
-        float horizontalMovement = Input.GetAxis( "Horizontal" );
-        float verticalMovement = Input.GetAxis( "Vertical" );
+		// Move about in the 2D area
+		float horizontalMovement = Input.GetAxis( "Horizontal" );
+		float verticalMovement = Input.GetAxis( "Vertical" );
 
-        Vector3 movement = new Vector3( speed * horizontalMovement, verticalVelocity, speed * verticalMovement );
-        controller.Move( rotation * movement * Time.deltaTime );
-		gameObject.transform.rotation = Quaternion.LookRotation (movement, Vector3.up);
-    }
+		Vector3 movement = new Vector3( speed * horizontalMovement, verticalVelocity, speed * verticalMovement );
+		controller.Move( rotation * movement * Time.deltaTime );
+	}
 
-    public override void Death()
-    {
-        Debug.Log( "Player died" );
-        //Awake();
-        GameObject.Find( "SceneController" ).GetComponent<SceneManagerScript>().reset();
-    }
+	public override void Death()
+	{
+		Debug.Log( "Player died" );
+		//Awake();
+		GameObject.Find( "SceneController" ).GetComponent<SceneManagerScript>().reset();
+	}
 }
