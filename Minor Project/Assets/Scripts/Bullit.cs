a1f017c0242a3f98be3a2245bdf8c7440f6610f0
Eugene Figueiras <e.figueiras1@gmail.com>
2016-11-23 16:49:23 +0100

    Turret toegevoegd die op je schiet als je binnen zijn line of sight komt.

---
+++ Minor Project/Assets/Scripts/Bullit.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Bullit : MonoBehaviour {
+	public float firespeed = 7.0f;
+	private Vector3 direction;
+	private GameObject Player;
+
+	// Use this for initialization
+	void Start () {
+
+		Player = GameObject.FindGameObjectsWithTag( "Player" )[ 0 ];
+		direction = (Player.transform.position - transform.position).normalized;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		transform.position += direction * (firespeed * Time.deltaTime);
+		Destroy (this.gameObject, 3.0f);
+	}
+}
---
+++ Minor Project/Assets/Scripts/Turret.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Turret : MonoBehaviour {
+	private GameObject Player;
+	public float LineofSight = 10f;
+	public float rotationSpeed = 3.0f;
+	private Vector3 startPos;
+	public GameObject bullit;
+	public GameObject bullitspawn;
+	private Quaternion startRot;
+
+	// Use this for initialization
+	void Start () {
+	 
+		startPos = gameObject.transform.position;
+		startRot = gameObject.transform.rotation;
+
+
+		InvokeRepeating("BullitTrigger",2f,1f);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Player = GameObject.FindGameObjectsWithTag( "Player" )[ 0 ];
+		Vector3 playerPos = Player.transform.position;
+		Vector3 objectPos = gameObject.transform.position;
+		Quaternion objectRot = gameObject.transform.rotation;;
+
+
+		if ( Vector3.Distance( playerPos, objectPos ) < LineofSight )
+		{
+			gameObject.transform.rotation = Quaternion.Slerp( objectRot, Quaternion.LookRotation( playerPos - objectPos ), rotationSpeed * Time.deltaTime );
+		}
+		else
+		{
+			gameObject.transform.rotation = Quaternion.Slerp( objectRot, startRot, rotationSpeed * Time.deltaTime );
+		}
+	}
+
+	public void BullitTrigger(){
+		Player = GameObject.FindGameObjectsWithTag( "Player" )[ 0 ];
+		Vector3 playerPos = Player.transform.position;
+		Vector3 objectPos = gameObject.transform.position;
+
+		if ( Vector3.Distance( playerPos, objectPos ) < LineofSight )
+		{
+			Instantiate (bullit, transform.position, transform.rotation);
+		}
+	}
+}
