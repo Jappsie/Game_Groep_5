45c6ab1ff9cbbdf5a0ff53750ea8e2c3c26da2b0
Mairin <mikroes38@gmail.com>
2016-11-24 22:01:13 +0100

    spawnen werkt nu volgens afspraak

--- Minor Project/Assets/Scripts/spawn.cs
+++ Minor Project/Assets/Scripts/spawn.cs
@@ -3,7 +3,7 @@ using System.Collections;
 
 public class spawn : MonoBehaviour {
 
-    // enemy objects
+    // list of enemy objects that can be spawned
     public GameObject[] enemies;
     // amount of enemies that need to be spawned
     public int amount;
@@ -11,41 +11,41 @@ public class spawn : MonoBehaviour {
     public float spawnTime;
     // time interval inbetween spawns in [s]
     public float interval;
-    // radius
+    // spawn radius
 	public float radius;
+    // spawn counter;
+    private int count;
+    // computed spawn location
 	private Vector3 spawnPoint;
 
-
+    // start spawning when object is activated
 	void Start() {
+        // initialize counter
+        count = 0;
 		InvokeRepeating ("spawnEnemy", spawnTime, interval);
 	}
 	
 	// Update is called once per frame
-	void Update () {
-        // identify all enemy objects in the world
-        //enemies = GameObject.FindGameObjectsWithTag("Enemy");
-        
+	void Update () {                
         // spawn until limit has been reached
-        if (enemies.Length >= amount)
+        if (count >= amount)
         {
-            // call spawn function
 			CancelInvoke ();
         }
-
 	}
 
     // spawn function
     void spawnEnemy ()
     {
+        count++;
         // randomly assign a coordinate within world space to the enemy
 		spawnPoint.x = Random.Range(-radius, radius);
 		spawnPoint.y = gameObject.transform.position.y;
 		spawnPoint.z = Random.Range(-radius, radius);
 
         // make an instance of a random enemy at the spawnPoint and align it with world coordinates
-		GameObject enemy = enemies[Random.Range(0, enemies.Length - 1)];
+		GameObject enemy = enemies[Random.Range(0, enemies.Length)];
 
 		Instantiate (enemy, gameObject.transform.position + spawnPoint + enemy.transform.position, Quaternion.identity);
-        // cancel the invoke and return
     }
 }
