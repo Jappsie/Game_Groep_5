b303050f030aa31e2ce50930f78be52506a19429
Jasper <jaspervanbemmelen@hotmail.com>
2016-12-05 18:40:13 +0100

    Teleporten werkt nu met exponentiele verdeling.

--- Minor Project/Assets/Scripts/EnemyFollowing.cs
+++ Minor Project/Assets/Scripts/EnemyFollowing.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 using System;
 
 public class EnemyFollowing : HealthSystem
@@ -8,14 +9,20 @@ public class EnemyFollowing : HealthSystem
     public float moveSpeed = 2.0f;
     public float rotationSpeed = 3.0f;
     public float Damage = 1;
+	public float teleportDistance = 0.8f;		//teleportDistance with respect to the player
+	public float teleportRate = 2f;				//Once every teleportRate seconds the enemy teleports (Exponential Distribution)
+	public bool canTeleport = false;
 
-    private GameObject Player;
+    private GameObject Player;					
     private Vector3 startPos;
+	//Teleport variables
+	private bool teleportReady;					//Boolean to be used with coroutine to check if enemy can teleport
 
     // Use this for initialization
     void Start()
     {
         startPos = gameObject.transform.position;
+		teleportReady = true;					//Enemy can teleport from the beginning
     }
 
     // Follow a 'player' if within followDistance
@@ -27,8 +34,18 @@ public class EnemyFollowing : HealthSystem
 		Vector3 playerPos = Player.transform.position;
 		Vector3 objectPos = gameObject.transform.position;
 		Quaternion objectRot = gameObject.transform.rotation;
+		while (teleportReady && canTeleport) {
+			teleportReady = false;
+			IEnumerator coroutine = Teleport ();
+			StartCoroutine(coroutine);
+		}
 
 		if (Vector3.Distance (playerPos, objectPos) < followDistance) {
+			while (teleportReady && canTeleport) {			//If ready to teleport and enabled, teleport
+				teleportReady = false;
+				IEnumerator coroutine = Teleport ();
+				StartCoroutine(coroutine);
+			}
 			gameObject.transform.rotation = Quaternion.Slerp (objectRot, Quaternion.LookRotation (playerPos - objectPos), rotationSpeed * Time.deltaTime);
 		} else {
 			gameObject.transform.rotation = Quaternion.Slerp (objectRot, Quaternion.LookRotation (startPos - objectPos), rotationSpeed * Time.deltaTime);
@@ -54,4 +71,21 @@ public class EnemyFollowing : HealthSystem
     {
         Destroy(gameObject);
     }
+
+	//Teleport method
+	private IEnumerator Teleport() {
+		float exponential = Uni2Exp (UnityEngine.Random.value, teleportRate);	//Draw exponentially distributed number (wait time)
+		yield return new WaitForSeconds( exponential );							//Wait for this long
+		Vector3 playerPosition = Player.gameObject.transform.position;  		//Player position
+		Quaternion playerRotation = Player.gameObject.transform.rotation;		//Player rotation
+		Vector3 teleportDirection = playerRotation * Vector3.back;				//Behind the player
+		Vector3 teleportLocation = teleportDirection * (gameObject.transform.position - playerPosition).magnitude * teleportDistance;	//Teleport location
+		gameObject.transform.position = teleportLocation + playerPosition;					//Apply teleport
+		teleportReady = true;													//Enemy is ready to teleport again
+	}
+
+	//Inverse transform method: F(x) = y => F^-1(y) = x
+	protected float Uni2Exp(float uniform, float lambda) {
+		return -(lambda) * Mathf.Log (uniform);					//if uniform~U(0,1) then for the returned value y: y~EXP(lambda) (lambda * exp( -lambda * y )
+	}
 }
