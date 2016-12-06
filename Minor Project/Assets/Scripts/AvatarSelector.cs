a3c9c50455c799c032f8da558e50607bf1d2d69e
Fieke Thijssen <f.m.w.thijssen@student.tudelft.nl>
2016-12-06 09:40:05 +0100

    Main menu & Pauze Menu. Wat nog niet optimaal werkt: Continue knop —> Hij schiet dan zijn bal af bij klik. Pause Menu en maincharacter worden niet Destroyed in Main Menu. Settings is nog niet geimplementeerd, zelfde geld voor Avatar selector.

---
+++ Minor Project/Assets/Scripts/AvatarSelector.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic; 
+
+public class AvatarSelector : MonoBehaviour {
+
+	private List<GameObject> models; 
+	private int selectionIndex = 0;
+
+	// Use this for initialization
+	void Start () {
+		
+		models = new List<GameObject> (); 
+		foreach (Transform t in transform) {
+			models.Add (t.gameObject);
+			t.gameObject.SetActive (false);
+		}
+		models [selectionIndex].SetActive (true);
+
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void Select(int index){
+		if (index == selectionIndex)
+			return; 
+		if (index < 0 || index >= models.Count)
+			return; 
+
+		models [selectionIndex].SetActive (false);
+		selectionIndex = index;
+		models [selectionIndex].SetActive (true);
+}
+
+}
---
+++ Minor Project/Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,80 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public class PauseMenu : MonoBehaviour {
+	
+GameObject[] pauseObjects;
+
+// Use this for initialization
+void Start () {
+	Time.timeScale = 1;
+	pauseObjects = GameObject.FindGameObjectsWithTag("ShowOnPause");
+	hidePaused();
+}
+
+// Update is called once per frame
+void Update () {
+
+	//uses the p button to pause and unpause the game
+	if(Input.GetKeyDown(KeyCode.P))
+	{
+		if(Time.timeScale == 1)
+		{
+			Time.timeScale = 0;
+			showPaused();
+		} else if (Time.timeScale == 0){
+			Debug.Log ("high");
+			Time.timeScale = 1;
+			hidePaused();
+		}
+	}
+}
+
+	//When continue button is pressed
+	public void Continue(){
+		Time.timeScale = 1;
+		hidePaused ();
+	}
+
+
+//Reloads the Level
+public void Reload(){
+	GameObject.Find( "SceneController" ).GetComponent<SceneManagerScript>().resetOnDeath();
+}
+
+//controls the pausing of the scene
+public void pauseControl(){
+	if(Time.timeScale == 1)
+	{
+		Time.timeScale = 0;
+		showPaused();
+	} else if (Time.timeScale == 0){
+		Time.timeScale = 1;
+		hidePaused();
+	}
+}
+
+//shows objects with ShowOnPause tag
+public void showPaused(){
+	foreach(GameObject g in pauseObjects){
+		g.SetActive(true);
+	}
+}
+
+//hides objects with ShowOnPause tag
+public void hidePaused(){
+	foreach(GameObject g in pauseObjects){
+		g.SetActive(false);
+	}
+}
+
+//Reloads main menu
+	public void LoadMainMenu(string level){
+		Application.LoadLevel (3);
+	}
+
+
+		
+
+} 
