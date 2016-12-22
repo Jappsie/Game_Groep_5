86a00d18a4d24ba1cbe339a77363d2123182537d
Eugene Figueiras <e.figueiras1@gmail.com>
2016-12-22 14:26:32 +0100

    QueenBee moving script

---
+++ Minor Project/Assets/Scripts/Enemy Behaviour/QueenBee.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class QueenBee : MonoBehaviour {
+	public float speed = 1.0f;
+	public int PingPongRange = 6;
+
+	private Vector3 pos1;
+	private Vector3 pos2;
+
+
+	// Use this for initialization
+	void Start () {
+		pos1 = new Vector3 (transform.position.x - PingPongRange, transform.position.y, transform.position.z);
+		pos2 = new Vector3 (transform.position.x + PingPongRange, transform.position.y, transform.position.z);
+	}
+
+	// Update is called once per frame
+	void Update () {
+		transform.position = Vector3.Lerp (pos1, pos2, (Mathf.Sin(speed * Time.time) + 1.0f) / 2.0f);
+	}
+}
