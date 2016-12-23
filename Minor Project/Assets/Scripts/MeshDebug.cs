a60d3290b871d935c34b1ea8b7d4a3334d3a3a8e
LudicrousLynx <a.r.zevenbergen@gmail.com>
2016-12-23 17:36:31 +0100

    still not working properly, but it's serviceable

---
+++ Minor Project/Assets/Scripts/MeshDebug.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MeshDebug : MonoBehaviour {
+	private Mesh mesh;
+	void Start () {
+		mesh = GetComponent<MeshFilter>().mesh;
+		Debug.Log (mesh.subMeshCount);
+		int[][] triangleList = new int[mesh.subMeshCount][];
+		for (int i = 0; i < mesh.subMeshCount; i++) {
+			triangleList [i] = mesh.GetTriangles (i);
+			Debug.Log (triangleList [i].Length);
+		}
+		for (int i = 0; i < mesh.subMeshCount-1; i++) {
+			foreach (int index in triangleList[1]) {
+				foreach (int index2 in triangleList[i+1]) {
+					if (mesh.vertices[index].Equals(mesh.vertices[index2])) {
+						Debug.Log("Duplicate: " + mesh.vertices[index]);
+					}
+				}
+			}
+		}
+
+	}
+
+
+}
