10828a0d98b5b56efdacd040ffa93132640ba39b
MPapendrecht <M.N.G.Papendrecht@student.tudelft.nl>
2016-12-23 20:54:01 +0100

    Het werkt!

---
+++ Minor Project/Assets/Scripts/Game Behaviour/GameManager.cs
@@ -0,0 +1,117 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Networking;
+using UnityEngine.SceneManagement;
+using System.Collections.Generic;
+
+public class GameManager: MonoBehaviour {
+
+    public float trackInterval = 2f;
+
+    private string UserName;
+    [SerializeField]
+    private int userId = -1;
+    [SerializeField]
+    private Scene currentScene;
+    [SerializeField]
+    private GameObject player;
+
+    private string glyph = "abcdefghijklmnopqrstuvwxyz0123456789";
+    private int glyphLength = 32;
+
+	// Use this for initialization
+	void Start () {
+        DontDestroyOnLoad( gameObject );
+        SceneManager.sceneLoaded += trackScene;
+        InvokeRepeating( "trackPlayer", 0, trackInterval );
+        if ( UserName == null )
+        {
+            SetUserName();
+        }
+	}
+
+    private void trackScene(Scene scene, LoadSceneMode mode)
+    {
+        Debug.Log( "Scene changed" );
+        if ( mode.Equals( LoadSceneMode.Single) )
+        {
+            currentScene = scene;
+            player = GameObject.FindGameObjectWithTag( "Player" );
+        }
+    }
+
+    private void trackPlayer()
+    {
+        if (player != null && userId != -1)
+        {
+            WWWForm form = new WWWForm();
+            form.AddField( "userId", userId );
+            form.AddField( "scene", currentScene.name );
+            form.AddField( "x", player.transform.position.x.ToString() );
+            form.AddField( "y", player.transform.position.y.ToString() );
+            form.AddField( "z", player.transform.position.z.ToString() );
+            StartCoroutine( sendData( "position", form ) );
+        }
+        else
+        {
+            trackScene(currentScene, LoadSceneMode.Single);
+        }
+    }
+
+    public void deadPlayer(Vector3 position)
+    {
+        if ( userId != -1 )
+        {
+            WWWForm form = new WWWForm();
+            form.AddField( "userId", userId );
+            form.AddField( "scene", currentScene.name );
+            form.AddField( "x", position.x.ToString() );
+            form.AddField( "y", position.y.ToString() );
+            form.AddField( "z", position.z.ToString() );
+            StartCoroutine( sendData( "death", form ) );
+        }
+    }
+
+    private void SetUserName()
+    {
+        UserName = "";
+        for (int i = 0; i < glyphLength; i++)
+        {
+            UserName += glyph[Random.Range(0,glyph.Length)];
+        }
+        StartCoroutine( newUser() );
+    }
+
+    IEnumerator newUser()
+    {
+        WWWForm form = new WWWForm();
+        form.AddField( "name", UserName );
+
+        WWW www = new WWW( "http://insyprojects.ewi.tudelft.nl:8085/newUser", form );
+
+        yield return www;
+
+        if (!string.IsNullOrEmpty(www.error))
+        {
+            Debug.Log( www.error );
+            CancelInvoke("trackPlayer");
+        }
+        else
+        {
+            JsonUtility.FromJsonOverwrite(www.text, this);
+        }
+    }
+
+    IEnumerator sendData(string url, WWWForm form)
+    {
+        Debug.Log("Sending data");
+        WWW www = new WWW( "http://insyprojects.ewi.tudelft.nl:8085/" + url, form );
+        yield return www;
+
+        if (!string.IsNullOrEmpty(www.error))
+        {
+            Debug.Log( www.error );
+        }
+    }
+	
+}
