2edf3447413b25c0898d1b0e179f2af0978a7658
MPapendrecht <M.N.G.Papendrecht@student.tudelft.nl>
2017-01-11 17:33:54 +0100

    Moved scenery to heatmap

--- Minor Project/Assets/Scripts/Game Behaviour/LoadData.cs
+++ Minor Project/Assets/Scripts/Game Behaviour/LoadData.cs
@@ -1,4 +1,5 @@
 ﻿using UnityEngine;
+using System.Collections.Generic;
 using System.Collections;
 using System.Text;
 using System.IO;
@@ -8,8 +9,10 @@ public class LoadData : MonoBehaviour {
     public GameObject sphere;
 	public GameObject skull;
 	public string level;
+    public bool death;
 
 	private Collider[] curColliders;
+    private List<Object> objects = new List<Object>();
 
 	// Use this for initialization
 	void Start () {
@@ -19,22 +22,38 @@ public class LoadData : MonoBehaviour {
 		float radius = sphere.transform.localScale.x;
         foreach (Data value in data)
         {
-			if (value.Scene.Equals (level) && value.Death == 0) {
+			if (value.Scene.Equals (level) && value.Death == 0 && !death) {
 				Vector3 position = new Vector3 (value.XPos, value.YPos, value.ZPos);
-				curColliders = Physics.OverlapSphere (position, radius);
+				curColliders = Physics.OverlapSphere (position, radius, 1 << 8);
 				foreach (Collider col in curColliders) {
-					col.gameObject.GetComponent<Renderer> ().material.color -= new Color (-3f / 255f, 3f / 255f, 0, -1f / 255f);
+                        col.gameObject.GetComponent<Renderer>().material.color -= new Color( -3f / 255f, 3f / 255f, 0, -1f / 255f );
 				}
-				GameObject.Instantiate (sphere, position, Quaternion.identity);
-			} else if (value.Scene.Equals (level) && value.Death == 1) {
+				objects.Add(GameObject.Instantiate (sphere, position, Quaternion.identity));
+			} else if (value.Scene.Equals (level) && value.Death == 1 && death) {
 				Vector3 position = new Vector3 (value.XPos, value.YPos, value.ZPos);
-				GameObject.Instantiate (skull, position, Quaternion.identity);
+				objects.Add(GameObject.Instantiate (skull, position, Quaternion.identity));
+
 			}
         }
 	}
 
-    
-	
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.H))
+        {
+            death = !death;
+            Debug.Log( objects.Count );
+            foreach (Object value in objects)
+            {
+                Destroy( value );
+            }
+            objects.Clear();
+            Start();
+        }
+    }
+
+
+
 }
 
 public class JSONHelper
