fd77a8ae1434bf3d6ef469d138294f48954666e3
MPapendrecht <M.N.G.Papendrecht@student.tudelft.nl>
2017-01-17 11:29:43 +0100

    Getwookerino'd

--- Minor Project/Assets/Scripts/Enemy Behaviour/StatueTurret.cs
+++ Minor Project/Assets/Scripts/Enemy Behaviour/StatueTurret.cs
@@ -11,6 +11,8 @@ public class StatueTurret : HealthSystem
     public float FieldOfView = 45f;
     public float activationTime = 1f;
     public float cooldownTime = 1f;
+    public bool adaptive = false;
+    public bool vulnerable = true;
 
     protected GameObject player;
     protected Vector3 startPos;
@@ -20,7 +22,13 @@ public class StatueTurret : HealthSystem
     protected Vector3 objectPos;
     protected Quaternion objectRot;
     protected Vector3 bulletPos;
+
+    private int deaths;
+    private double repeatrate;
     private EnableScript toggler;
+    private Renderer rendererStatue;
+    private Color color;
+    private Color flicker;
 
     // Get the player object
     protected virtual void Start()
@@ -29,6 +37,17 @@ public class StatueTurret : HealthSystem
         startPos = gameObject.transform.position;
         startRot = gameObject.transform.rotation;
         toggler = GetComponent<EnableScript>();
+        if ( adaptive )
+        {
+            deaths = SceneManagerScript.deathList.Count;        
+            //When deaths == 0, repeatrate is equal to itself
+            activationTime = 2f * activationTime * (1 / (1 + Mathf.Exp( -0.3f * deaths )));
+            cooldownTime = 2f * cooldownTime * (1 / (1 + Mathf.Exp( -0.3f * deaths )));
+        }
+        rendererStatue = gameObject.GetComponent<Renderer>();
+        color = rendererStatue.material.color;
+        flicker = color;
+        flicker.a = 0.2f;
     }
 
     // Always look at the player
@@ -81,10 +100,13 @@ public class StatueTurret : HealthSystem
     {
         if (collision.gameObject.CompareTag("PlayerBullet"))
         {
-            TakeDamage( 1f );
             Destroy( collision.gameObject );
+            if ( vulnerable )
+            {
+                TakeDamage( 1f );
+                StartCoroutine( "Flicker" );
+            }
         }
-        
     }
 
     // What happens when the object dies
@@ -96,5 +118,20 @@ public class StatueTurret : HealthSystem
             toggler.toggle( true );
         }
         Destroy( gameObject );
+    }
+
+    IEnumerator Flicker()
+    {
+        vulnerable = false;
+        Debug.Log( "Invulnerable: " + Time.time );
+        for ( int i = 0; i < 8; i++ )
+        {
+            rendererStatue.material.color = flicker;
+            yield return new WaitForSeconds( 0.05f );
+            rendererStatue.material.color = color;
+            yield return new WaitForSeconds( 0.05f );
+        }
+        vulnerable = true;
+        Debug.Log( "Vulnerable: " + Time.time );
     }
 }
