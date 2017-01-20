a8af00a32a14a32ce0ec687d1efc7ca2921565cf
LudicrousLynx <a.r.zevenbergen@gmail.com>
2017-01-20 21:02:45 +0100

    snek commit

---
+++ Minor Project/Assets/Scripts/Enemy Behaviour/snekMovement.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NewBehaviourScript : MonoBehaviour {
+
+	public float speed;
+	public float strikeDistance;
+	public float remoteDistance;
+
+	private GameObject Player;
+	private GameObject Snek;
+	private bool strike;
+	private Vector3 playerPosition;
+
+	// UNUSED
+	void Start () {
+		Snek = this.gameObject;
+	}
+	
+
+	void FixedUpdate () {
+		Player = GameObject.FindGameObjectWithTag ("Player");
+		playerPosition = Player.transform.position;
+		if (strike) {
+			Snek.transform.position = playerPosition.MoveTowards(
+		}
+	}
+}
