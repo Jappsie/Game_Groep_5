551a8f646bbff3abcae28c5d42dd32e05323a1a6
Mairin <mikroes38@gmail.com>
2017-01-23 14:18:02 +0100

    Animatie toegevoed voor het droppen van de zaag

---
+++ Minor Project/Assets/Scripts/Game Behaviour/SawDroppingAnim.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SawDroppingAnim : MonoBehaviour {
+
+    // dropping animatie
+    Animator anim;
+
+	// Use this for initialization
+	void Start () {
+        anim = this.GetComponent<Animator>();
+        anim.Play("SawDropping");
+	}
+
+    void OnTriggerEnter(Collider col)
+    {
+        // if collision with player
+        if (col.gameObject.tag == "Player")
+        {
+            // align movement with world coordinates
+            anim.applyRootMotion = true;
+            // and destroy this script
+            Destroy(this);
+        }
+    }
+}
