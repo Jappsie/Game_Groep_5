840535f6754e4f9dc1378fc3219e8e151b2b0114
Mairin <mikroes38@gmail.com>
2017-01-23 14:53:22 +0100

    Target pulseert nu om meer aandacht te trekken

--- Minor Project/Assets/Scripts/Game Behaviour/SawSpawner.cs
+++ Minor Project/Assets/Scripts/Game Behaviour/SawSpawner.cs
@@ -3,11 +3,18 @@ using System.Collections;
 
 public class SawSpawner : EnableScript {
 
+    Animator anim;
+
     private void OnCollisionEnter( Collision collision )
     {
         if ( collision.gameObject.CompareTag( "PlayerBullet" ) )
         {
             toggle( true );
+            anim = this.GetComponent<Animator>();
+            // stop animating the target when hit
+            anim.SetTime(0);
+            Destroy(anim);
+
         }
     }
 }
