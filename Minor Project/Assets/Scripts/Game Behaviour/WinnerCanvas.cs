d21015a559874343eec174bde3ddf4d4e79e46f3
Fieke Thijssen <f.m.w.thijssen@student.tudelft.nl>
2017-01-24 21:20:49 +0100

    WinnerCanvas nu wel goed?

---
+++ Minor Project/Assets/Scripts/Game Behaviour/WinnerCanvas.cs
@@ -0,0 +1,43 @@
+﻿	using UnityEngine;
+	using System.Collections;
+	using UnityEngine.UI;
+
+	public class WinnerCanvas : snakeScript {
+
+	public Canvas Winner; 
+	public Button Credits;
+
+
+		public MovieTexture movie; 
+
+		// Use this for initialization
+		void Start () {
+		
+			GetComponent<RawImage> ().texture = movie as MovieTexture; 
+			movie.loop = true;
+			movie.Play (); 
+		Winner = Winner.GetComponent<Canvas> (); 
+		Credits = Winner.GetComponent<Button> (); 
+
+		Winner.enabled = false; 
+		Credits.enabled = false; 
+		}
+
+		public void GoToCredits(){ 
+
+			SceneManagerScript.goToScene("Credits", false );
+		}
+
+		// Update is called once per frame
+		void Update () {
+
+		if (CurHealth <= 0) {
+			Time.timeScale = 0; 
+			Winner.enabled = true; 
+			Credits.enabled = true; 
+	
+		}
+
+		}
+
+}
