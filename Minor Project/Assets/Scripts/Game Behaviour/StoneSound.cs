255d4a081b162f22119b2d66dc5793bb809bb8cc
Mairin <mikroes38@gmail.com>
2017-01-24 22:03:48 +0100

    Sound effects en background muziek toegevoegd

---
+++ Minor Project/Assets/Scripts/Game Behaviour/StoneSound.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StoneSound : MonoBehaviour {
+
+    AudioSource audio;
+    Animator anim;
+
+	// Use this for initialization
+	void Start () {
+        audio = GetComponent<AudioSource>();
+	}
+	
+	// Update is called once per frame
+	void OnTriggerEnter (Collider col) {
+	    if (col.CompareTag("TurretBullet"))
+        {
+            audio.Play();
+            Destroy(this);
+        }
+	}
+}
--- Minor Project/Assets/Scripts/Game Behaviour/treeFall.cs
+++ Minor Project/Assets/Scripts/Game Behaviour/treeFall.cs
@@ -3,14 +3,18 @@ using System.Collections;
 
 public class treeFall : MonoBehaviour {
 	Animator animator;
+    AudioSource audio;
 	// Use this for initialization
 	void Start () {
 		animator = GetComponent<Animator> ();
+        audio = GetComponent<AudioSource>();
 	}
 
 	private void OnTriggerEnter(Collider col){
 		if (col.CompareTag("saw")) {
 			animator.Play ("FallingTree");
+            audio.Play();
+            Destroy(this);
 		}
 	} 
 }
