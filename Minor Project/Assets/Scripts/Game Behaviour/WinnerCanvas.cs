efa22f4c4ee6a0d2bd41a521bc4ec38f14167c58
Fieke Thijssen <f.m.w.thijssen@student.tudelft.nl>
2017-01-24 18:19:31 +0100

    Winner filmpje op canvas

---
+++ Minor Project/Assets/Scripts/Game Behaviour/WinnerCanvas.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class WinnerCanvas : snakeScript {
+
+
+	public MovieTexture movie; 
+	private GameObject snake; 
+	public Canvas Winner; 
+	public Button Credits;
+
+	// Use this for initialization
+	void Start () {
+		GetComponent<RawImage> ().texture = movie as MovieTexture; 
+		movie.loop = true;
+		movie.Play (); 
+
+	
+		snake = GameObject.FindGameObjectWithTag ("SnakeController");
+		Winner = Winner.GetComponent<Canvas> ();
+		Credits = Credits.GetComponent<Button> (); 
+
+		Winner.enabled = false; 
+		Credits.enabled = false; 
+
+	}
+		
+	void GoToCredits(){ 
+
+		SceneManagerScript.goToScene( "Credits", false );
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (CurHealth <= 0) {
+			Winner.enabled = true; 
+			Credits.enabled = true; 
+			Time.timeScale = 0; 
+
+		}
+	
+	}
+}
