edc67a7c3113c1e18f3fb2c728afdecfebea8d91
Mairin <mikroes38@gmail.com>
2017-01-26 13:35:12 +0100

    Poging day-night cycle

---
+++ Minor Project/Assets/Scripts/Game Behaviour/DayNightCycle.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DayNightCycle : MonoBehaviour {
+
+    public Vector3 center = new Vector3(0, 0, 0);
+    public Vector3 direction = Vector3.right;
+    public float movementSpeed;
+    private float cycle;
+
+	// Use this for initialization
+	void Start () {
+        cycle = movementSpeed * Time.deltaTime;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.RotateAround(center, direction, cycle);
+        transform.LookAt(center);
+	}
+}
