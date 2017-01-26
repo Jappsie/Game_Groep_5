d8c98fc372b856de0fee1ad5e44742bf502a4644
mikroes <mikroes38@gmail.com>
2017-01-26 16:00:19 +0100

    Nu day/night met Advanced Lighting en Reflections, Ambient Occlusion en ambient lighting in Level 1

--- Minor Project/Assets/Scripts/Game Behaviour/DayNightCycle.cs
+++ Minor Project/Assets/Scripts/Game Behaviour/DayNightCycle.cs
@@ -4,7 +4,7 @@ using System.Collections;
 public class DayNightCycle : MonoBehaviour {
 
     public Vector3 center = new Vector3(0, 0, 0);
-    public Vector3 direction = Vector3.right;
+    public Vector3 direction = Vector3.left;
     public float movementSpeed;
     private float cycle;
 
