c681d4cfcd755d0171c6b930dcf939ddd80342c7
Mairin <mikroes38@gmail.com>
2017-01-27 14:26:01 +0100

    Kleine bug in day/night rotatie, neem deze commit als je ook de 47MB aan belichting data niet wilt

--- Minor Project/Assets/Scripts/Game Behaviour/DayNightCycle.cs
+++ Minor Project/Assets/Scripts/Game Behaviour/DayNightCycle.cs
@@ -3,13 +3,16 @@ using System.Collections;
 
 public class DayNightCycle : MonoBehaviour {
 
-    public Vector3 center = new Vector3(0, 0, 0);
-    public Vector3 direction = Vector3.left;
+	public Vector3 center;
+	public Vector3 direction;
     public float movementSpeed;
     private float cycle;
 
 	// Use this for initialization
 	void Start () {
+		center = new Vector3 (0, 0, 0);
+		direction = Vector3.left;
+		movementSpeed = 4f;
         cycle = movementSpeed * Time.deltaTime;
 	}
 	
