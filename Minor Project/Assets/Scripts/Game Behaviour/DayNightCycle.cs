22a7e2bdc6b5b1a256eb00bb702f7c168ae56a4a
mikroes <mikroes38@gmail.com>
2017-01-28 19:09:56 +0100

    Day/nigh cycle aangepast. "Lekt" nu geen licht meer

--- Minor Project/Assets/Scripts/Game Behaviour/DayNightCycle.cs
+++ Minor Project/Assets/Scripts/Game Behaviour/DayNightCycle.cs
@@ -3,19 +3,49 @@ using System.Collections;
 
 public class DayNightCycle : MonoBehaviour {
 
-    public Vector3 center = new Vector3(0, 0, 0);
-    public Vector3 direction = Vector3.right;
+	public Camera mc;
+    public Vector3 center;
+    public Vector3 direction;
     public float movementSpeed;
+	private Light light;
     private float cycle;
+	public float posX;
+
+	public Color dayBack;
+	public Color nightBack;
+
+	public bool newDay;
+	public Color curColor;
 
 	// Use this for initialization
 	void Start () {
+		mc = GameObject.Find("Main Camera").GetComponent<Camera>();
+		newDay = false;
+
+		//dayBack = new Color(206, 241, 255);
+		//nightBack = new Color(23, 20, 62);
+
+		light = this.GetComponent<Light>();
+		center = new Vector3(0,0,0);
         cycle = movementSpeed * Time.deltaTime;
+		direction = Vector3.left;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        transform.RotateAround(center, direction, cycle);
+        
+		posX = transform.eulerAngles.x;
+
+		if ((posX < 360f && posX > 270f)  && light.intensity > 0f) {
+			light.intensity += -0.001f;
+			mc.backgroundColor = Color.Lerp(nightBack, mc.backgroundColor, 0.99f);
+			
+		}			
+		if ((posX > 0 && posX < 90f) && light.intensity < 1.0f) {
+			light.intensity += 0.001f;
+			mc.backgroundColor = Color.Lerp(dayBack, mc.backgroundColor, 0.99f);
+		}
+		transform.RotateAround(center, direction, cycle);
         transform.LookAt(center);
 	}
 }
