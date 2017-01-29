534138f4ec27f78c9a23930f86b3b86c9e7f4e67
mikroes <mikroes38@gmail.com>
2017-01-29 18:22:59 +0100

    opnieuw de niet gebugde versie van day/night gecommit

--- Minor Project/Assets/Scripts/Game Behaviour/DayNightCycle.cs
+++ Minor Project/Assets/Scripts/Game Behaviour/DayNightCycle.cs
@@ -4,29 +4,34 @@ using System.Collections;
 public class DayNightCycle : MonoBehaviour {
 
 	public Camera mc;
+
+	// Rotation parameters
+
     public Vector3 center;
     public Vector3 direction;
     public float movementSpeed;
-	private Light light;
-	private Light light2;
-    private float cycle;
+	private float cycle;
 	public float posX;
-	public float moonlightBrightness;
 
+	// sun
+	private Light light;
+	// moon
+	private Light light2;
+	// moonlight brightness	
+	public float moonlightBrightness = 0.27f;
+	// camera backgrounds for day and night
 	public Color dayBack;
 	public Color nightBack;
-
 	public Color curColor;
 
 	// Use this for initialization
 	void Start () {
-		mc = GameObject.Find("Main Camera").GetComponent<Camera>();
-		light2 = GameObject.Find("Moonlight").GetComponent<Light>();
-
 		//dayBack = new Color(206, 241, 255);
 		//nightBack = new Color(23, 20, 62);
-
+		RenderSettings.ambientIntensity = 1.0f;
+		mc = GameObject.Find("Main Camera").GetComponent<Camera>();
 		light = this.GetComponent<Light>();
+		light2 = GameObject.Find("Moonlight").GetComponent<Light>();
 		light2.intensity = 0f;
 		center = new Vector3(0,0,0);
         cycle = movementSpeed * Time.deltaTime;
@@ -36,25 +41,35 @@ public class DayNightCycle : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
         
+		// current angular position of the sun
 		posX = transform.eulerAngles.x;
 
-		if ((posX < 360f && posX > 270f)  && light.intensity > 0f) {
-			light.intensity += -0.01f;
+		// Case for sunset
+		if ((posX > 270f && posX < 360f)  && light.intensity > 0f) {
+			// decrease sun intensity
+			light.intensity -= 0.01f;
+			// make transition to night sky
 			mc.backgroundColor = Color.Lerp(nightBack, mc.backgroundColor, 0.9f);
 			if (light.intensity < 0.2f && light2.intensity < moonlightBrightness) {
 				light2.intensity += 0.01f;
-				RenderSettings.ambientIntensity = 0.3f;
+				if (RenderSettings.ambientIntensity > 0.2f)
+					RenderSettings.ambientIntensity -= 0.1f;
 			}
 			
-		}			
+		}
+		// Case for sunrise			
 		if ((posX > 0 && posX < 90f) && light.intensity < 0.9f) {
+			// increase sun intensity
 			light.intensity += 0.01f;
+			// make transition to daylight
 			mc.backgroundColor = Color.Lerp(dayBack, mc.backgroundColor, 0.9f);
 			if (light.intensity > 0.9 && light2.intensity > 0) {
 				light2.intensity -= 0.01f;
-				RenderSettings.ambientIntensity = 0.3f;
+				if (RenderSettings.ambientIntensity < 1.0f)
+					RenderSettings.ambientIntensity += 0.1f;
 			}
 		}
+		// always move around the center and point in the direction of it
 		transform.RotateAround(center, direction, cycle);
         transform.LookAt(center);
 	}
